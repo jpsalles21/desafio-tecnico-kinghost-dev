d3b928e530696a9a177a69dfd0d7e71e47775d64
jpsalles21 <j.salles001@edu.pucrs.br>
2024-07-30 17:15:10 -0300

    feat: adicionado componente que lista os estados por meio de um dropdown para selecionar

---
+++ src/components/States/states.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { fetchUserStates } from '../../api/api';
+import './style.css';
+
+const States = ({ onStateSelect }) => {
+    const [states, setStates] = useState([]);
+    const [selectedState, setSelectedState] = useState('');
+
+    useEffect(() => {
+        const getStates = async () => {
+            try {
+                const fetchedStates = await fetchUserStates();
+                setStates(fetchedStates);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        getStates();
+    }, []);
+
+    const handleChange = (event) => {
+        const state = event.target.value;
+        setSelectedState(state);
+        onStateSelect(state);
+    };
+
+    return (
+        <div className="states">
+            <div className="select-container">
+                <h2>Estados</h2>
+                <select
+                    className="select-dropdown"
+                    value={selectedState}
+                    onChange={handleChange}
+                >
+                    <option value="" disabled>Selecione um estado</option>
+                    {states.map((state, index) => (
+                        <option key={index} value={state}>
+                            {state}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default States;
