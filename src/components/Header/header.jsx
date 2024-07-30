428fc0d50ce010ec838a1a25c6e036edb0810372
jpsalles21 <j.salles001@edu.pucrs.br>
2024-07-30 17:14:41 -0300

    feat: adicionado componente header

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Header = () => {
+    return (
+        <header className="header-container">
+            <h1 className="header-title">Desafio Técnico - KingHost - João Pedro Salles </h1>
+        </header>
+    )
+}
+
+export default Header;
