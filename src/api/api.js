283451f3ac827c9131acf0d41f000b9a3a993d6e
jpsalles21 <j.salles001@edu.pucrs.br>
2024-07-31 01:28:38 -0300

    refactor: removendo código desnecessário

--- src/api/api.js
+++ src/api/api.js
@@ -24,17 +24,6 @@ export const fetchUsersByState = async (state) => {
     }
 };
 
-export const fetchUserImages = async () => {
-    try {
-        const response = await axios.get(url);
-        const users = response.data.users;
-        return users.map(user => ({id: user.id, image: user.image}));
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-};
-
 const getUniqueSortedStates = (users) => {
     const uniqueStates = new Set(users.map(user => user.address.state));
     return [...uniqueStates].sort((a, b) => a.localeCompare(b));
